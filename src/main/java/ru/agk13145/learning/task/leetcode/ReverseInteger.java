6b177d6f8f857776c42aee720b68491e46eb18b9
Alexander Kuznetsov <agk13145@icloud.com>
2020-12-12 14:27:37 +0300

    ReverseInteger and ReverseIntegerTest is added

--- src/main/java/ru/agk13145/learning/task/leetcode/ReverseInteger.java
+++ src/main/java/ru/agk13145/learning/task/leetcode/ReverseInteger.java
@@ -1,2 +1,31 @@
-package ru.agk13145.learning.task.leetcode;public class ReverseInteger {
+package ru.agk13145.learning.task.leetcode;
+/**
+ * https://leetcode.com/problems/reverse-integer/
+ */
+public class ReverseInteger {
+
+    /**
+     * retrieve deployed integer
+     * @param x integer for reverse
+     * @return
+     */
+    public int reverse(int x) {
+        int reverseX = 0;
+        while (x != 0) {
+            int n = x%10;
+            x = x/10;
+
+            if (reverseX > (Integer.MAX_VALUE-Math.abs(n))/10) {
+                return 0;
+            }
+            if (reverseX < (Integer.MIN_VALUE+Math.abs(n))/10) {
+                return 0;
+            }
+
+            reverseX = reverseX * 10 + n;
+
+        }
+        return reverseX;
+    }
+
 }
--- src/test/java/ru/agk13145/learning/task/leetcode/ReverseIntegerTest.java
+++ src/test/java/ru/agk13145/learning/task/leetcode/ReverseIntegerTest.java
@@ -1,2 +1,32 @@
-package ru.agk13145.learning.task.leetcode;public class ReverseIntegerTest {
+package ru.agk13145.learning.task.leetcode;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ReverseIntegerTest {
+    ReverseInteger reverseInteger;
+
+    @BeforeAll
+    static void initializationBeforeClass() {
+        System.out.println("Begin test ReverseInteger");
+    }
+
+    @BeforeEach
+    void initBeforeEachFunction() {
+        reverseInteger = new ReverseInteger();
+    }
+
+    @DisplayName("Test #1")
+    @Test
+    void testReverseInteger() {
+        int x = 3461;
+        int excepted = 1643;
+        assertEquals(excepted, reverseInteger.reverse(x));
+    }
+
+
 }
